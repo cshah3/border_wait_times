414bc7454490fe513bac972b2e19408dbdf6dad2
jcarrharris <joncarrharris@gmail.com>
2013-08-30 18:39:57 -0400

    added

---
+++ site/Gemfile
@@ -0,0 +1,5 @@
+# A sample Gemfile
+source :rubygems
+gem 'rack'
+
+# gem "rails"
--- site/public/border_wait_times.rb
+++ site/public/border_wait_times.rb
@@ -2,6 +2,6 @@ require 'erb'
 
 template = ERB.new(File.read("border_times.html.erb"))
 
-File.open('border.html', 'w') do |f|
+File.open('index.html', 'w') do |f|
 	f.write template.result
 end
